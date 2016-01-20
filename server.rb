716288c84ab53075f11c265f5433ce1670fe3979
Jared Murphy <jaredcmurphy@gmail.com>
2016-01-20 18:53:02 -0500

    updated seeds

--- server.rb
+++ server.rb
@@ -71,122 +71,29 @@ module IowaMusic
   	get '/create_entry' do
   		@title = "create entry"
   		
-  		@genres = conn.exec("SELECT * FROM genres;")
+  	
   		@categories = entries.map { |a| a.to_s }
 
-  		erb :form
+  		erb :create_entry
   	end
     
+    get '/create_entry/albums' do
+      erb :albums
+    end
 
-    # CREATE POST
-  	post "/create_entry" do
-      category = params["category"]
-
+    post '/create_entry/albums' do
       a_name = params["a_name"]
-      a_name = params["a_band"]
+      a_band = params["a_band"]
       a_img_url = params["a_img_url"]
       a_listen_url = params["a_listen_url"]
       a_description = params["a_description"]
 
-    	b_name = params["b_name"]
-    	b_img_url = params["b_img_url"]
-    	b_genre_one = params["b_genre_one"]
-    	b_location = params["b_location"]
-    	b_description = params["b_description"]
-    	b_website_url = params["b_website_url"]
-
-      f_name = params["f_name"]
-      f_img_url = params["f_img_url"]
-      f_location = params["f_location"]
-      f_description = params["f_description"]
-      f_website_url = params["f_website_url"]
-
-      l_name = params["l_name"]
-      l_img_url = params["l_img_url"]
-      l_location = params["l_location"]
-      l_description = params["l_description"]
-      l_website_url = params["l_website_url"]
-      l_albums = params["l_albums"]
-      l_bands = params["l_songs"]
-
-      rs_name = params["rs_name"]
-      rs_img_url = params["rs_img_url"]
-      rs_location = params["rs_location"]
-      rs_description = params["rs_description"]
-      rs_website_url = params["rs_website_url"]
-
-
-      song_name = params["song_name"]
-      song_band = params["song_band"]
-      song_album = params["song_album"]
-      song_lyrics = params["song_lyrics"]
-      song_description = params["song_description"]
-   
-      studio_name = params["studio_name"]
-      studio_img_url = params["studio_img_url"]
-      studio_location = params["studio_location"]
-      studio_description = params["studio_description"]
-      studio_website_url = params["studio_website_url"]
-      
-      
-      v_name = params["v_name"]
-      v_img_url = params["v_img_url"]
-      v_location = params["v_location"]
-      v_description = params["v_description"]
-      v_website_url = params["v_website_url"]
-    	
-      if category == 'Albums'
-        conn.exec_params("INSERT INTO albums (name, band, img_url, listen_url, description) 
+      conn.exec_params("INSERT INTO albums (name, band, img_url, listen_url, description) 
           VALUES ($1, $2, $3, $4, $5)",
           [a_name, a_band, a_img_url, a_listen_url, a_description]
         )
-    	elsif category == 'Bands'
-      	conn.exec_params(
-      		"INSERT INTO bands (name, img_url, genre_one, location, description, website_url) 
-      		VALUES ($1, $2, $3, $4, $5, $6)",
-      		[b_name, b_img_url, b_genre_one, b_location, b_description, b_website_url]
-      	)
+      erb :albums
 
-      elsif category == 'Festivals'
-        conn.exec_params(
-          "INSERT INTO festivals (name, img_url, location, description, website_url) 
-          VALUES ($1, $2, $3, $4, $5)",
-          [f_name, f_img_url, f_location, f_description, f_website_url]
-        )
-      elsif category == 'Labels'
-        conn.exec_params(
-          "INSERT INTO labels (name, img_url, bands, albums, location, website_url, description) 
-          VALUES ($1, $2, $3, $4, $5, $6, $7)",
-          [l_name, l_img_url, l_bands, l_albums, l_location, l_website_url, l_description]
-        )
-      elsif category == 'Record_stores'
-        conn.exec_params(
-          "INSERT INTO record_stores (name, img_url, location, description, website_url) 
-          VALUES ($1, $2, $3, $4, $5)",
-          [rs_name, rs_img_url, rs_location, rs_description, rs_website_url]
-        )
-      elsif category == 'Songs'
-        conn.exec_params(
-          "INSERT INTO songs (name, band, album, lyrics, description) 
-          VALUES ($1, $2, $3)",
-          [song_name, song_band, song_album, song_lyrics, song_description]
-        )
-        elsif category == 'Studios'
-        conn.exec_params(
-          "INSERT INTO studios (name, img_url, location, description, website_url) 
-          VALUES ($1, $2, $3, $4, $5)",
-          [studio_name, studio_img_url, studio_location, studio_description, studio_website_url]
-        )
-        elsif category == 'Venues'
-        conn.exec_params(
-          "INSERT INTO venues (name, img_url, location, description, website_url) 
-          VALUES ($1, $2, $3, $4, $5)",
-          [v_name, v_img_url, v_location, v_description, v_website_url]
-        )
-      end
-
-    	@entry_submitted = true
-    	erb :form
     end
 
     # end create post
