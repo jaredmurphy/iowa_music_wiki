3b8f52d9cd1a585b1c16c753888aaca83e9a2bc5
Jared Murphy <jaredcmurphy@gmail.com>
2016-01-20 13:53:18 -0500

    changed bands var names on serer and form

--- server.rb
+++ server.rb
@@ -70,13 +70,13 @@ module IowaMusic
   	end
 
   	post "/create_entry" do
-    	name = params["name"]
-    	img_url = params["img_url"]
-    	genre_one = params["genre_one"]
-    	location = params["location"]
-    	description = params["description"]
-    	website_url = params["website_url"]
-      category = params["category"]
+    	b_name = params["name"]
+    	b_img_url = params["img_url"]
+    	b_genre_one = params["genre_one"]
+    	b_location = params["location"]
+    	b_description = params["description"]
+    	b_website_url = params["website_url"]
+      b_category = params["category"]
       v_name = params["v_name"]
       v_img_url = params["v_img_url"]
       v_location = params["v_location"]
@@ -87,7 +87,7 @@ module IowaMusic
       	conn.exec_params(
       		"INSERT INTO bands (name, img_url, genre_one, location, description, website_url) 
       		VALUES ($1, $2, $3, $4, $5, $6)",
-      		[name, img_url, genre_one, location, description, website_url]
+      		[b_name, b_img_url, b_genre_one, b_location, b_description, b_website_url]
       	)
       
         elsif category == 'Venues'
