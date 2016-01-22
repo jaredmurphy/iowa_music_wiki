74996ee64a57e324f145513d22165e1444230224
Jared Murphy <jaredcmurphy@gmail.com>
2016-01-22 15:51:48 -0500

    adds red carpet, author id and time on article page

--- lib/seeds.rb
+++ lib/seeds.rb
@@ -55,38 +55,6 @@ conn.exec("CREATE TABLE bands(
   )"
 )
 
-	conn.exec("INSERT INTO bands (name, img_url, genre_one, location, description, website_url, edited) VALUES (
-	      'Mighty Shady',
-	      'https://scontent-lga3-1.xx.fbcdn.net/hphotos-xtp1/v/t1.0-9/11703137_881388171896197_2019369173197765871_n.jpg?oh=348ec4f02948f6276fa6572dad3ec0aa&oe=573BFCCB',
-	      'rock', 
-	      'Des Moines', 
-	      'Mighty Shady was awesome', 
-	      'https://www.facebook.com/mightyshadymusic/',
-	      CURRENT_TIMESTAMP
-		  )"
-	)
-
-	conn.exec("INSERT INTO bands (name, img_url, genre_one, location, description, website_url, edited) VALUES (
-	    'Fire Sale', 
-	    'http://www.campeuforia.com/wp-content/uploads/2010/02/Firesalepic.jpg',
-	    'funk', 
-	    'Davenport', 
-	    'Fire Sale was awesome', 
-	    'https://www.facebook.com/firesaleband/',
-	    CURRENT_TIMESTAMP
-		)"
-	)
-
-	conn.exec("INSERT INTO bands (name, img_url, genre_one, location, description, website_url, edited) VALUES (
-	    'The Candymakers', 
-	    'https://scontent-lga3-1.xx.fbcdn.net/hphotos-xta1/v/t1.0-9/12036662_1123911297638969_8377565350024694590_n.jpg?oh=329ffc7046a7abb3df6870d19775239b&oe=5741533F',
-	    'funk', 
-	    'Davenport', 
-	    'Ridiculicious', 
-	    'http://www.thecandymakers.com/',
-	    CURRENT_TIMESTAMP
-		)"
-	)
 
 conn.exec("DROP TABLE IF EXISTS edits")
 conn.exec("CREATE TABLE edits(
